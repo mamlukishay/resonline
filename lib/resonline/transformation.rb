96c95395ff8a9305860c4cc2ec114cfe1f2c3755
Rijaludin Muhsin <rijaludinmuhsin@gmail.com>
2014-08-12 15:36:36 +0700

    Transformation service > SetAvailability

--- lib/resonline/transformation.rb
+++ lib/resonline/transformation.rb
@@ -47,6 +47,48 @@ module Resonline
       return Resonline::ErrorMessages.exception(e)
     end
 
+    def self.set_availability(room_id, start_date, end_date, number_available)
+      client = Savon.client(soap_options)
+      xml = '
+        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns="http://www.bookeasy.com/ws/api/2.1.0.0/">
+          <soapenv:Header/> 
+          <soapenv:Body> 
+            <ns:SetAvailability> 
+               <ns:XmlParameters><![CDATA[ 
+                  <BookEasy> 
+                    <Operator> 
+                      <Credentials> 
+                        <Username>' + Resonline.configuration.username + '</Username> 
+                        <Password>' + Resonline.configuration.password + '</Password> 
+                      </Credentials> 
+                      <Rooms> 
+                        <Room> 
+                          <RoomID>' + room_id.to_s + '</RoomID> 
+                          <Availabilities> 
+                            <Availability> 
+                              <StartDate>' + start_date.strftime("%Y-%m-%d") + '</StartDate> 
+                              <EndDate>' + end_date.strftime("%Y-%m-%d") + '</EndDate> 
+                              <NumberAvailable>' + number_available.to_s + '</NumberAvailable> 
+                            </Availability> 
+                          </Availabilities> 
+                        </Room> 
+                      </Rooms> 
+                    </Operator> 
+                  </BookEasy> 
+                  ]]></ns:XmlParameters> 
+            </ns:SetAvailability>
+          </soapenv:Body> 
+        </soapenv:Envelope>'
+      response = client.call(:set_availability, xml: xml)
+      if response.success?
+        return response.body[:set_availability_response][:set_availability_result]
+      else
+        return Resonline::ErrorMessages.failed_response(response)
+      end
+    rescue Exception => e
+      return Resonline::ErrorMessages.exception(e)
+    end
+
   end
 
 end

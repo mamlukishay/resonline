9d98b075d09647abc16826e15789fa56d0ce7a77
Lutvi Rosyady <moorails@gmail.com>
2015-04-23 14:47:52 +0700

    UPdate Notification

--- lib/resonline/notification.rb
+++ lib/resonline/notification.rb
@@ -27,27 +27,48 @@ module Resonline
 					            <ns1:HotelAuthenticationKey></ns1:HotelAuthenticationKey>
 					            <dir:Booking>
 					               <ns:BookingDetails>
-					                  <ns:Adults>'+ options[:number_of_guests] +'</ns:Adults>
-					                  <ns:CheckIn>'+ options[:checkin].try(:xmlschema) +'</ns:CheckIn>
-					                  <ns:CheckOut>'+ options[:checkout].try(:xmlschema) +'</ns:CheckOut>
-					                  <ns:Children>'+ options[:children] +'</ns:Children>
+					                  <ns:Adults>' + options[:number_of_guests] + '</ns:Adults>
+					                  <ns:CheckIn>' + options[:checkin].try(:xmlschema) + '</ns:CheckIn>
+					                  <ns:CheckOut>' + options[:checkout].try(:xmlschema) + '</ns:CheckOut>
+					                  <ns:Children>' + options[:children] + '</ns:Children>
 					                  <!--Optional:-->
-					                  <ns:CurrencyISO>'+ options[:currency] +'</ns:CurrencyISO>
-					                  <ns:Infants>'+ options[:infants] +'</ns:Infants>
+					                  <ns:CurrencyISO>' + options[:currency] + '</ns:CurrencyISO>
+					                  <ns:Infants>' + options[:infants] + '</ns:Infants>
 					                  <!--Optional:-->
-					                  <ns:OwingAmount>'+ options[:total_amount] +'</ns:OwingAmount>
+					                  <ns:OwingAmount>' + options[:total_amount] +'</ns:OwingAmount>
 					                  <ns:PropertyId>' + Resonline.configuration.hotel_id.to_s + '</ns:PropertyId>
-					                  <ns:RoomCode>'+ options[:room_code] +'</ns:RoomCode>
+					                  <ns:RoomCode>' + options[:room_code] + '</ns:RoomCode>
 					                  <!--Optional:-->
-					                  <ns:Quantity>'+ options[:quantity] +'</ns:Quantity>
+					                  <ns:Quantity>' + options[:quantity] + '</ns:Quantity>
 					                  <!--Optional:-->
-					                  <ns:SpecialRequests>'+ options[:special_request] +'</ns:SpecialRequests>
+					                  <ns:SpecialRequests>' + options[:special_request] + '</ns:SpecialRequests>
 					               </ns:BookingDetails>
 					               <!--Optional:-->
-					               <ns:BookingRef>'+ options[:booking_reference] +'</ns:BookingRef>
+					               <ns:BookingRef>' + options[:booking_reference] + '</ns:BookingRef>
 					               <!--Optional:-->
-					               <ns:BookingType>'+ options[:booking_reference] +'</ns:BookingType>
+					               <ns:BookingType>' + options[:booking_type] + '</ns:BookingType>
 					               <!--Optional:-->
+					               <ns:CreditCardDetails>
+					                  <ns:ExpiryDate>' + options[:cc_expired].try(:xmlschema) + '</ns:ExpiryDate>
+					                  <ns:HolderFullName>' + options[:full_name] + '</ns:HolderFullName>
+					                  <ns:Number>' + options[:cc_number] + '</ns:Number>
+					                  <ns:VerificationNumber>' + options[:cc_verification_number] + '</ns:VerificationNumber>
+					               </ns:CreditCardDetails>
+					               <ns:MainGuest>
+					                  <ns:AddressFirstLine>' + options[:address_1] + '</ns:AddressFirstLine>
+					                  <!--Optional:-->
+					                  <ns:AddressSecondLine>' + options[:address_2] + '</ns:AddressSecondLine>
+					                  <ns:City>' + options[:city] + '</ns:City>
+					                  <ns:CountryIso>' + options[:country_iso] + '</ns:CountryIso>
+					                  <ns:Email>' + options[:email] + '</ns:Email>
+					                  <ns:FirstName>' + options[:first_name] + '</ns:FirstName>
+					                  <ns:LastName>' + options[:last_name] + '</ns:LastName>
+					                  <!--Optional:-->
+					                  <ns:Mobile>' + options[:mobile] + '</ns:Mobile>
+					                  <ns:Phone>' + options[:phone] + '</ns:Phone>
+					                  <ns:Postcode>' + options[:postcode] + '</ns:Postcode>
+					                  <ns:State>' + options[:state] + '</ns:State>
+					               </ns:MainGuest>
 					            </dir:Booking>
 					            <dir:RequestType>API</dir:RequestType>
 					         </ns:request>
@@ -55,7 +76,7 @@ module Resonline
 					   </soapenv:Body>
 					</soapenv:Envelope>'
 			response = client.call(:booking_notification, xml: xml)
-			response.body[:booking_notification_response]
+			response.body[:booking_notification_response][:booking_notification_result][:response][:booking_response]
 		end
 	end
 end
